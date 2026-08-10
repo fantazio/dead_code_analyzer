5315080e190173a888db927cd0f7878153abc451
Corentin De Souza <9597216+fantazio@users.noreply.github.com>
2026-08-10 12:45:33 +0200

    [src] cleanup export-related code

--- src/deadSign.mli
+++ src/deadSign.mli
@@ -1,40 +1,41 @@
-type context =
-  | Toplevel
-  | In_module of (Ident.t * Location.t)
-  | Include
-
-val collect_export :
-  context:context ->
+val collect_export_from_signature :
   path:Ident.t list ->
   comp_unit:string ->
-  stock:(Lexing.position, string * string) Hashtbl.t ->
-  Types.signature_item
+  Typedtree.signature
   -> unit
-(** [collect_export ~context ~path ~comp_unit ~stock sig_item]
-    recursively traverse the [sig_item] and store exported elements
-    (values, constructors, ...) in [stock].
+(** [collect_export_from_signature ~path ~comp_unit signature]
+    recursively traverse the [signature] items and store exported elements
+    (values, constructors, ...) in {!DeadCommon.decs} (or equivalent).
     - [path] is the current module path. E.g. for a value in a submodule,
       the path is [Module.Submodule].
     - [comp_unit] is the compilation unit of the [sig_item].
-    - [context] specifies if the current signature item is found at the
-      [Toplevel] of the current compilation unit, in a module, a modtype,
-      or an [Include]
+*)
+
+val collect_export_from_structure :
+  path:Ident.t list ->
+  comp_unit:string ->
+  Typedtree.structure
+  -> unit
+(** [collect_export_from_structure ~path ~comp_unit structure]
+    recursively traverse the [structure] items and store exported elements
+    (values, constructors, ...) in {!DeadCommon.decs} (or equivalent).
+    See {!collect_export_from_signature} above for more information.
+*)
+
+val collect_export_from_include :
+  path:Ident.t list ->
+  comp_unit:string ->
+  Types.signature_item
+  -> unit
+(** [collect_export_from_include ~path ~comp_unit sig_item]
+    recursively traverse the [sig_item] and store exported elements
+    (values, constructors, ...) in {!DeadCommon.incl}.
+    See {!collect_export_from_signature} above for more information.
 *)
 
 val correct_export : Types.signature_item -> unit
 (** Unexport the signature_item. This is used to correct wrongful exports from
     {!collect_export} above
-*)
-
-val collect_export_from_typedtree :
-  path:Ident.t list ->
-  comp_unit:string ->
-  Typedtree.signature
-  -> unit
-(** [collect_export_from_typedtree ~path ~comp_unit sigature]
-    recursively traverse the [signature] items and store exported elements
-    (values, constructors, ...) in {!DeadCommon.desc}.
-    See {!collect_export} above for more information.
 *)
 
 val modtype :
@@ -45,9 +46,3 @@ val modtype :
     for [mt] agree. I.e. if the first one is explicit when the second one is.
     If not, then the first one is implicit and [on_mismatch] is called on the
     second. *)
-
-val collect_export_from_structure :
-  path:Ident.t list ->
-  comp_unit:string ->
-  Typedtree.structure
-  -> unit
