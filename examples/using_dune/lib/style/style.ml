a45c60e596c806f2510d713c4c712d482ad3696c
Corentin De Souza <9597216+fantazio@users.noreply.github.com>
2026-09-03 14:55:17 +0200

    [examples][using_dune][style] add more useless binding
    The `let (x : t) = ...` case produces a different tree from
    `let x = ...`. The former is translated to `let ((_ : t) as x) = ...`.
    The particular case of `let x : t = ...` is either equivalent to the
    former (from OCaml 5.1 to 5.4) or the latter.
    The former is equivalent to the latter in OCaml 5.5.

--- examples/using_dune/lib/style/style.ml
+++ examples/using_dune/lib/style/style.ml
@@ -5,3 +5,18 @@ let all =
   let useless_binding = 42 in
   useless_binding
 
+(* more useless bindings *)
+
+let _ =
+  let constrained_useless_binding : int = 42 in
+  (* This is translated as
+     let ((_ : int) as constrained_useless_binding) = ...
+     from OCaml 5.1 to 5.4 *)
+  constrained_useless_binding
+
+let _ =
+  let (constrained_useless_binding : int) = 42 in
+  (* This is translated as
+     let ((_ : int) as constrained_useless_binding) = ...
+     OCaml <= 5.4 *)
+  constrained_useless_binding
